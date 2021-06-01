95b783ca26018dd039161a9fe3e576379d55d72d
B22-User <gguljemalg@gmail.com>
2021-06-01 12:04:12 -0400

    added more

---
+++ src/day48_constructors_static/static_examples/CalculatorTest.java
@@ -0,0 +1,12 @@
+package day48_constructors_static.static_examples;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+
+        Calculator.add(5,3);
+
+        Calculator calc = new Calculator();
+        calc.multiply(2,4);
+    }
+}
