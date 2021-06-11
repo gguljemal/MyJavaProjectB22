cdbacbb311190aeb364fb7d32e4364486749260c
B22-User <gguljemalg@gmail.com>
2021-06-10 21:47:44 -0400

    added my second class

---
+++ src/day51_inheritance/Constructor.java
@@ -0,0 +1,10 @@
+package day51_inheritance;
+
+public class Constructor extends Employee {
+
+    public double calculateSalary(double hourlyRate) {
+
+        return 50 * 40 * hourlyRate;
+    }
+
+}
