b627d858c022cda6806f8de7482f11bc791b22f6
B22-User <gguljemalg@gmail.com>
2021-06-10 21:42:24 -0400

    added method to the Employee class

--- src/day51_inheritance/Employee.java
+++ src/day51_inheritance/Employee.java
@@ -1,4 +1,9 @@
 package day51_inheritance;
 
 public class Employee {
+    String jobTitle;
+
+    public double calculateSalary(double hourlyRate) {
+        return 52 * 40 * hourlyRate *1.1;
+    }
 }
