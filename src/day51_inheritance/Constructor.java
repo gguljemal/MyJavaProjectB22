ad1add337a2ef7836df18572f1f69c31b48bf698
B22-User <gguljemalg@gmail.com>
2021-06-10 22:36:23 -0400

    added my third class with methods

--- src/day51_inheritance/Constructor.java
+++ src/day51_inheritance/Constructor.java
@@ -2,9 +2,19 @@ package day51_inheritance;
 
 public class Constructor extends Employee {
 
+    //1) lets everyone know that you are overriding this method
+    //2) ensures that this method is being overridden. if not, it shows ERROR
+    @Override
     public double calculateSalary(double hourlyRate) {
 
         return 50 * 40 * hourlyRate;
     }
 
+
+    @Override
+    public String toString() {
+        return "Constructor{" +
+                "jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
 }
--- src/day51_inheritance/Employee.java
+++ src/day51_inheritance/Employee.java
@@ -6,4 +6,12 @@ public class Employee {
     public double calculateSalary(double hourlyRate) {
         return 52 * 40 * hourlyRate *1.1;
     }
+
+    //toString method is inherited from Object class
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
 }
