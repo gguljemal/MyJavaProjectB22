f722818b75b557e3f02e9ab615723ba9308b2c80
B22-User <gguljemalg@gmail.com>
2021-06-17 15:15:40 -0400

    added new package explain hidden static method

--- src/day53_inheritance/tesla/ModelX.java
+++ src/day53_inheritance/tesla/ModelX.java
@@ -1,5 +1,7 @@
 package day53_inheritance.tesla;
 
+import day53_inheritance.tesla.ElectricCar;
+
 public class ModelX extends ElectricCar {
 
     public ModelX(String make, String model, double price, int year, int range) {
--- src/day53_inheritance/tesla/Tesla.java
+++ src/day53_inheritance/tesla/Tesla.java
@@ -1,5 +1,7 @@
 package day53_inheritance.tesla;
 
+import day53_inheritance.tesla.ElectricCar;
+
 public class Tesla extends ElectricCar {
 
     public Tesla(String model, double price, int year, int range) {
