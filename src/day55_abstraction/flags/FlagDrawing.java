2517fb4c6b6d3e66880c0ec7f75112a86d5d9616
B22-User <gguljemalg@gmail.com>
2021-06-23 12:24:01 -0400

    some more parent class and concrete class examples

---
+++ src/day55_abstraction/flags/FlagDrawing.java
@@ -0,0 +1,18 @@
+package day55_abstraction.flags;
+
+import java.awt.*;
+
+public class FlagDrawing {
+
+    public static void main(String[] args) {
+
+        Grid grid = new Grid();
+
+//        for (int i = 0; i < grid.getHt(); i++) {
+//            grid.setColor(i, 0, Color.GREEN);
+//        }
+
+        RussianFlag russianFlag = new RussianFlag();
+        russianFlag.draw();
+    }
+}
