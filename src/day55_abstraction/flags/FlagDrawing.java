49f9aa3522cb94760f6acf0c3e43d56a2f95da2d
B22-User <gguljemalg@gmail.com>
2021-06-23 12:36:27 -0400

    created some objects

--- src/day55_abstraction/flags/FlagDrawing.java
+++ src/day55_abstraction/flags/FlagDrawing.java
@@ -14,5 +14,11 @@ public class FlagDrawing {
 
         RussianFlag russianFlag = new RussianFlag();
         russianFlag.draw();
+
+        FrenchFlag frenchFlag = new FrenchFlag();
+        frenchFlag.draw();
+
+        UkrainianFlag ukrainianFlag = new UkrainianFlag();
+        ukrainianFlag.draw();
     }
 }
