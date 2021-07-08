4cb53cfe7e636766fd7af49c3a21ec3f62e7fe1a
B22-User <gguljemalg@gmail.com>
2021-07-08 12:50:09 -0400

    finish my practice in exception topic

--- src/day60_exceptions/ThrowingException.java
+++ src/day60_exceptions/ThrowingException.java
@@ -1,4 +1,19 @@
 package day60_exceptions;
 
 public class ThrowingException {
+
+    public static void main(String[] args) {
+
+        System.out.println("lets create exceptions");
+
+       // RuntimeException e = new RuntimeException();
+       // throw e;
+        throw new RuntimeException();
+
+        String userName = "";
+        if (userName.isEmpty()){
+            throw new RuntimeException("Username cannot be empty");
+        }
+
+    }
 }
