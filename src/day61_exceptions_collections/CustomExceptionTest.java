4cf0f9db6099bded5116c72aa3215fb688bcbc75
B22-User <gguljemalg@gmail.com>
2021-07-14 11:59:18 -0400

    throw and throws  explanation with practices

---
+++ src/day61_exceptions_collections/CustomExceptionTest.java
@@ -0,0 +1,24 @@
+package day61_exceptions_collections;
+
+public class CustomExceptionTest {
+
+    public static void main(String[] args) {
+
+        int minutes = 60;
+        System.out.println("Class is going on for " + minutes + " minutes");
+        if (minutes > 50) {
+            throw new BreakTimeException("It is break time!");
+        }
+        System.out.println("Lets continue the class");
+
+        double balance = 400;
+        double itemPrice = 500;
+
+        System.out.println("I have $" + balance + " and purchasing item for $" + itemPrice);
+        if (itemPrice > balance) {
+            throw new InsufficientBalanceException("Transaction declined. Not enough funds :(");
+        }
+
+        System.out.println("Item successfully purchased");
+    }
+}
